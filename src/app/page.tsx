52736ca27cd0d1f182f1df101a87fd609f48b96d
Ghost Typer <edwinolivares123@gmail.com>
2025-06-24 16:40:53 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import Hero from "@/components/hero";
 import { FaHome } from "react-icons/fa";
+import Grid from "@/components/grid";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-white dark:bg-black flex justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,5 +1,36 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          ),
+        )}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
