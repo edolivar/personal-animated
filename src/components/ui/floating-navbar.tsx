a36acfe479b814140c863697a631c575e1f4e818
Ghost Typer <edwinolivares123@gmail.com>
2025-06-26 11:47:46 -0700

    update nav on mobile

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -57,7 +57,7 @@ export const FloatingNav = ({
         }}
         className={cn(
           "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 px-8 py-2  items-center justify-center space-x-4",
-          className,
+          className
         )}
       >
         {navItems.map((navItem: any, idx: number) => (
@@ -69,7 +69,7 @@ export const FloatingNav = ({
               if (navItem.link.startsWith("#")) {
                 e.preventDefault();
                 const element = document.querySelector(
-                  navItem.link,
+                  navItem.link
                 ) as HTMLElement | null;
                 if (element) {
                   element.scrollIntoView({
@@ -81,11 +81,11 @@ export const FloatingNav = ({
               // Let Next.js handle regular navigation for other links
             }}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500",
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="block text-sm">{navItem.name}</span>
           </Link>
         ))}{" "}
       </motion.div>
